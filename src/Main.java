52b9de21014f3cea6435bdb5c6df9aa34e242432
Nicolas <nicolasgfalabella@gmail.com>
2025-06-07 16:53:28 -0300

    Logica del Main ( Falta logica de las funciones )

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,90 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import models.Pedido;
+import models.Producto;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    private static ArrayList<Producto> productos = new ArrayList<>() ;
+    private static ArrayList<Pedido> pedidos = new ArrayList<>() ;
+    private static Scanner scanner = new Scanner(System.in) ;
+
+    private static void mostrarMenu() {
+        System.out.println("\n===== MENÚ PRINCIPAL =====");
+        System.out.println("1. Agregar producto");
+        System.out.println("2. Listar productos");
+        System.out.println("3. Buscar/Actualizar producto");
+        System.out.println("4. Eliminar producto");
+        System.out.println("5. Crear un pedido");
+        System.out.println("6. Listar pedidos");
+        System.out.println("7. Salir");
+    }
+
+    private static int leerEntero(String mensaje) {
+        System.out.println("Leer entero");
+        return 15;
+    }
+
+    private static void agregarProducto() {
+        System.out.println("Agregar Producto");
+    }
+
+    private static void listarProductos() {
+        System.out.println("Listar Productos");
+    }
+
+    private static void actualizarProducto() {
+        System.out.println("Actualizar Producto");
+    }
+
+    private static void eliminarProducto() {
+        System.out.println("Eliminar Producto");
+    }
+
+    private static void crearPedido() {
+        System.out.println("Crear Producto");
+    }
+
+    private static void listarPedidos() {
+        System.out.println("Listar Producto");
+    }
+
+
+
+    public static void main(String[] args) {
+        int opcion ;
+
+        do {
+            mostrarMenu() ;
+            opcion = leerEntero("Elegir una opcion : ") ;
+
+            switch (opcion) {
+                case 1 :
+                    agregarProducto() ;
+                    break ;
+                case 2 :
+                    listarProductos() ;
+                    break;
+                case 3 :
+                    actualizarProducto() ;
+                    break;
+                case 4 :
+                    eliminarProducto() ;
+                    break ;
+                case 5 :
+                    crearPedido() ;
+                    break ;
+                case 6 :
+                    listarPedidos() ;
+                    break ;
+                case 7 :
+                    System.out.println("¡Gracias por usar el sistema! Hasta luego.");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Probá de nuevo.");
+            }
+        } while ( opcion != 7) ;
     }
 }
+
